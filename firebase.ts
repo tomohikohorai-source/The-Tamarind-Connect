cc0648bd90abbc157325a2a385fab10b76dbaaf3
tomohikohorai-source <tomohiko.horai@gmail.com>
2026-01-11 18:29:23 +0900

    Create firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getFirestore, collection, addDoc, updateDoc, deleteDoc, doc, onSnapshot, query, orderBy } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBh6tz0moZgu9f346ECjFBAzw_-KSCiTQ",
+  authDomain: "the-tamarind-connect.firebaseapp.com",
+  projectId: "the-tamarind-connect",
+  storageBucket: "the-tamarind-connect.firebasestorage.app",
+  messagingSenderId: "936861918762",
+  appId: "1:936861918762:web:73c991a8a4449812118553",
+  measurementId: "G-XCK1G5KLQX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export { collection, addDoc, updateDoc, deleteDoc, doc, onSnapshot, query, orderBy };
